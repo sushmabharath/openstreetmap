24334e85cd4ff822ca69baa78b2ada30c4225981
sushmabharath <sushmacv12@gmail.com>
2017-09-06 12:43:11 -0400

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+The submission consists of 5 files:
+1. Sample osm file is generated with k = 1000
+2. audit.py is used to audit the data. The code in this file is designed to generate csv files for the sample file provided.
+3. clean.py includes helper functions in addition to the code used to audit the data. That is, it audits the data and cleans the mistakes.
+4. Openstreet map pdf is a report that explains the work done in the project.
+5. References file 
---
+++ clean.py
@@ -0,0 +1,204 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import csv
+import codecs
+import pprint
+import re
+import xml.etree.cElementTree as ET
+
+import cerberus
+
+import schema
+
+OSM_PATH = "sample_1000ny.osm"
+
+NODES_PATH = "nodes.csv"
+NODE_TAGS_PATH = "nodes_tags.csv"
+WAYS_PATH = "ways.csv"
+WAY_NODES_PATH = "ways_nodes.csv"
+WAY_TAGS_PATH = "ways_tags.csv"
+
+LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
+PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+SCHEMA = schema.schema
+
+mapping = { "Ave": "Avenue",
+            "avenue": "Avenue",
+            "Ctr": "Counter",
+			"Cir": "Circle",
+			"Cres": "Crescent",
+			"north":"North",
+			"Pkwy":"Parkway"
+            }
+
+# Make sure the fields order in the csvs matches the column order in the sql table schema
+NODE_FIELDS = ['id', 'lat', 'lon', 'user', 'uid', 'version', 'changeset', 'timestamp']
+NODE_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
+WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
+WAY_NODES_FIELDS = ['id', 'node_id', 'position']
+
+
+def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
+                  problem_chars=PROBLEMCHARS, default_tag_type='regular'):
+    """Clean and shape node or way XML element to Python dict"""
+
+    node_attribs = {}
+    way_attribs = {}
+    way_nodes = []
+    tags = []  # Handle secondary tags the same way for both node and way elements
+
+    if element.tag == "node" or element.tag == "way":
+        for t in element.iter("tag"):
+            chk = problem_chars.search(t.attrib['k'])
+            if chk:
+                continue
+            else:
+                tag = {}
+                tag["id"] = element.attrib["id"]                
+                m = LOWER_COLON.search(t.attrib["k"])
+                if m:
+                    val = t.attrib["k"].split(":",1)
+                    tag["key"] = val[1]
+                    tag["type"] = val[0]
+                    if 'street' in t.attrib["k"]:
+                        tag["value"] = update_name(t.attrib["v"],mapping)
+                    else:
+                        tag["value"] = t.attrib["v"]
+                else:
+                    tag["key"] = t.attrib["k"]
+                    tag["type"] = default_tag_type
+                    if 'phone' in t.attrib["k"]:
+                        if update_phone(t.attrib["v"]):
+                            tag["value"] = update_phone(t.attrib["v"])
+                        else:
+                            continue
+                    else:
+                        tag["value"] = t.attrib["v"]
+                    
+            tags.append(tag)
+            
+    if element.tag == "node":
+        for i in range(0,len(node_attr_fields)):
+            node_attribs[node_attr_fields[i]] = element.attrib[node_attr_fields[i]]
+            
+    if element.tag == "way":
+        for i in range(0,len(way_attr_fields)):
+            way_attribs[way_attr_fields[i]] = element.attrib[way_attr_fields[i]]
+            
+        counter = 0 
+        for elem in element.iter("nd"):
+            way = {}
+            way["id"] = element.attrib["id"]
+            way["node_id"] = elem.attrib["ref"]
+            way["position"] = counter
+            way_nodes.append(way)
+            counter +=1
+            
+        
+                
+    if element.tag == 'node':
+        return {'node': node_attribs, 'node_tags': tags}
+    elif element.tag == 'way':
+        return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
+
+
+# ================================================== #
+#               Helper Functions                     #
+# ================================================== #
+def update_name(name, mapping):
+    m = street_type_re.search(name)
+    if m:
+        street_type = m.group()
+        if street_type in mapping.keys():
+            name = re.sub(street_type, mapping[street_type], name)
+    return name
+	
+def update_phone(phone):
+    phone1 = re.sub(r'\D', "", phone)
+    phone2 = re.match(r'^(\d{3})(\d{3})(\d{4})$', phone1)
+    if phone2:
+        return "(" + phone2.group(1) + ") " + phone2.group(2) + "-" + phone2.group(3)
+    
+	
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag"""
+
+    context = ET.iterparse(osm_file, events=('start', 'end'))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+def validate_element(element, validator, schema=SCHEMA):
+    """Raise ValidationError if element does not match schema"""
+    if validator.validate(element, schema) is not True:
+        field, errors = next(validator.errors.iteritems())
+        message_string = "\nElement of type '{0}' has the following errors:\n{1}"
+        error_string = pprint.pformat(errors)
+        
+        raise Exception(message_string.format(field, error_string))
+
+
+class UnicodeDictWriter(csv.DictWriter, object):
+    """Extend csv.DictWriter to handle Unicode input"""
+
+    def writerow(self, row):
+        super(UnicodeDictWriter, self).writerow({
+            k: (v.encode('utf-8') if isinstance(v, unicode) else v) for k, v in row.iteritems()
+        })
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+# ================================================== #
+#               Main Function                        #
+# ================================================== #
+def process_map(file_in, validate):
+    """Iteratively process each XML element and write to csv(s)"""
+
+    with codecs.open(NODES_PATH, 'w') as nodes_file,          codecs.open(NODE_TAGS_PATH, 'w') as nodes_tags_file,          codecs.open(WAYS_PATH, 'w') as ways_file,          codecs.open(WAY_NODES_PATH, 'w') as way_nodes_file,          codecs.open(WAY_TAGS_PATH, 'w') as way_tags_file:
+
+        nodes_writer = UnicodeDictWriter(nodes_file, NODE_FIELDS)
+        node_tags_writer = UnicodeDictWriter(nodes_tags_file, NODE_TAGS_FIELDS)
+        ways_writer = UnicodeDictWriter(ways_file, WAY_FIELDS)
+        way_nodes_writer = UnicodeDictWriter(way_nodes_file, WAY_NODES_FIELDS)
+        way_tags_writer = UnicodeDictWriter(way_tags_file, WAY_TAGS_FIELDS)
+
+        nodes_writer.writeheader()
+        node_tags_writer.writeheader()
+        ways_writer.writeheader()
+        way_nodes_writer.writeheader()
+        way_tags_writer.writeheader()
+
+        validator = cerberus.Validator()
+
+        for element in get_element(file_in, tags=('node', 'way')):
+            el = shape_element(element)
+            if el:
+                if validate is True:
+                    validate_element(el, validator)
+
+                if element.tag == 'node':
+                    nodes_writer.writerow(el['node'])
+                    node_tags_writer.writerows(el['node_tags'])
+                elif element.tag == 'way':
+                    ways_writer.writerow(el['way'])
+                    way_nodes_writer.writerows(el['way_nodes'])
+                    way_tags_writer.writerows(el['way_tags'])
+
+
+if __name__ == '__main__':
+    # Note: Validation is ~ 10X slower. For the project consider using a small
+    # sample of the map when validating.
+    process_map(OSM_PATH, validate=False)
+
